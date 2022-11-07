b570cf26fcd4fff6ae3e5642c4e4960bdca34534
Yagnik <59044382+Yagnik-Gohil@users.noreply.github.com>
2022-11-07 18:18:40 +0530

    Added Like button

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,24 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import { BsCartPlusFill } from 'react-icons/bs'
 import { BiDetail } from 'react-icons/bi'
+import { FcLikePlaceholder, FcLike } from 'react-icons/fc'
 
 function Card() {
+  const [isLiked, setIsLiked] = useState(false)
+
+  const handleLike = () => {
+    if (isLiked) {
+      setIsLiked(false)
+    }
+    else {
+      setIsLiked(true)
+    }
+
+  }
   return (
     <Fragment>
       <div className='col-lg-3 col-md-4 col-sm-6 pt-3 d-flex justify-content-center aligh-items-center'>
-        <div className="card" style={{ "width": "18rem" }}>
+        <div className="card p-2" style={{ "width": "18rem" }}>
           <img src="https://m.media-amazon.com/images/I/911j2oCRNpL._AC_UL480_QL65_.jpg" className="card-img-top item-image" alt="..." />
           <div className="card-body">
             <h5 className="card-title">Hyper realistic lion pencil drawing</h5>
@@ -15,9 +27,14 @@ function Card() {
               <span className='text-decoration-line-through'>₹1,000</span>&nbsp;(20% off)
             </div>
           </div>
-          <div className='d-flex aligh-items-start'>
-            <a href="#" className="btn text-white m-1 bg-blue card-btn"><BsCartPlusFill /> Add to cart</a>
-            <a href="#" className="btn text-white m-1 bg-blue card-btn"><BiDetail /> View Details</a>
+          <div className='d-flex align-items-center justify-content-center flex-wrap'>
+            <a href="#" className="btn text-white m-1 bg-blue card-btn fs-12"><BsCartPlusFill /> Add to cart</a>
+            <a href="#" className="btn text-white m-1 bg-blue card-btn fs-12"><BiDetail /> View Details</a>
+            {
+              isLiked ?
+              <span className='p-2'><FcLike onClick={handleLike}/></span> :
+              <span className='p-2'><FcLikePlaceholder onClick={handleLike}/></span>
+            }
           </div>
         </div>
       </div>
