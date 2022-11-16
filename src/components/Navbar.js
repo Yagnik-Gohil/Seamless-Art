c7306e56b0826101fd7f7350098bc1b56351a0db
Yagnik <59044382+Yagnik-Gohil@users.noreply.github.com>
2022-11-16 14:59:11 +0530

    Logout functionality implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,60 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import { useLocation } from 'react-router-dom';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { BsCart } from 'react-icons/bs';
 import ModalItem from './ModalItem'
+import axios from "axios";
 
 function Navbar() {
+
+    const userEmail = localStorage.getItem('user');
     const route = useLocation();
-    
+    const navigate = useNavigate();
+
+    const [show, setShow] = useState(false);
+    const [type, setType] = useState("primary");
+    const [alertMessage, setAlertMessage] = useState()
+
+    const logout = async () => {
+        try {
+            const res = await axios({
+                method: "GET",
+                url: "http://localhost:8000/api/v1/user/logout",
+            });
+            if (res.data.status === "success") {
+                ShowAlert("success", "Log out successfully!");
+                window.setTimeout(() => {
+                    navigate("/login");
+                }, 1000);
+                localStorage.clear()
+            }
+        } catch (err) {
+            // console.log(err)
+            ShowAlert("danger", "Error logging out! Try again.");
+        }
+    }
+    function ShowAlert(type, message) {
+        setType(type)
+        setAlertMessage(message);
+        setShow(true)
+        window.setTimeout(() => {
+            setShow(false);
+            setAlertMessage("");
+        }, 1000);
+    }
     return (
         <Fragment>
+            {
+                show &&
+                <div className='alert-parent'>
+                    <div className={`alert alert-${type}`} role="alert">
+                        {alertMessage}
+                        <div className="progress mt-2 bg-white">
+                            <div className={`progress-bar progress-bar-striped bg-${type} progress-bar-animated fill-1`} role="progressbar" aria-label="Animated striped example" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
+            }
             <nav className="navbar navbar-expand-md navbar-light bg-light top-navbar">
                 <div className="container-fluid">
                     <a className="navbar-brand">Seamless Art</a>
@@ -24,23 +70,28 @@ function Navbar() {
                                     <BsCart size={20} data-bs-toggle="modal" data-bs-target="#exampleModal" />
                                 </li>
                             }
-                            <li className="nav-item dropdown">
-                                <button type="button" className="btn nav-link dropdown-toggle fw-bold" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Yagnik Gohil
-                                </button >
-                                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <li><Link className="dropdown-item" to="/account">My Account</Link></li>
-                                    <li><Link className="dropdown-item" to="/orders">My Orders</Link></li>
-                                    {/* <li><Link className="dropdown-item" to="/home">Favourits</Link></li> */}
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><Link className="dropdown-item" to="/home">Log Out</Link></li>
-                                </ul>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/login" className="nav-link fw-bold" role="button">
-                                    Login
-                                </Link >
-                            </li>
+                            {
+                                userEmail &&
+                                <li className="nav-item dropdown">
+                                    <button type="button" className="btn nav-link dropdown-toggle fw-bold" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        {userEmail}
+                                    </button >
+                                    <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                        <li><Link className="dropdown-item" to="/account">My Account</Link></li>
+                                        <li><Link className="dropdown-item" to="/orders">My Orders</Link></li>
+                                        {/* <li><Link className="dropdown-item" to="/home">Favourits</Link></li> */}
+                                        <li><hr className="dropdown-divider" /></li>
+                                        <li><span className="dropdown-item" onClick={logout}>Log Out</span></li>
+                                    </ul>
+                                </li>
+                            }
+                            {!userEmail &&
+                                <li className="nav-item">
+                                    <Link to="/login" className="nav-link fw-bold" role="button">
+                                        Login
+                                    </Link >
+                                </li>
+                            }
                         </ul>
                     </div>
                 </div>
